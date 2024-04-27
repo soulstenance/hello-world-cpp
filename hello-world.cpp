6cd1623783ce6d43ba4db26c6e3c8691587e6345
soulstenance <soulstenance.e2pis@slmail.me>
2024-04-27 14:18:34 -0400

    Declared namespace

--- hello-world.cpp
+++ hello-world.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
+// We declare the std namespace now, in lieu of declaring it repeatedly throughout the code.
+using namespace std;
+
 int main() {
-    std::cout << "Hello World!" << std::endl;
+    cout << "Hello World!" << endl << "We are in the already declared 'std' namespace!" << endl;
     return 0;
 }
